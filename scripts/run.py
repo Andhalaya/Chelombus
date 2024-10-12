a7de702d599922f1cfaba68e46fad196843da76f
alforesep <afloresep01@gmail.com>
2024-10-12 20:48:17 +0200

    update ipynb and run update

--- scripts/run.py
+++ scripts/run.py
@@ -17,46 +17,48 @@ def main():
     reducer = DimensionalityReducer()
     output_gen = OutputGenerator()
 
-    # Calculate total number of chunks (optional)
+    # Calculate total number of chunks (optional, for tqdm)
     total_chunks = data_handler.get_total_chunks(DATA_FILE_PATH, CHUNKSIZE)
 
     # Load data in chunks
     data_chunks = data_handler.load_data(DATA_FILE_PATH, CHUNKSIZE)
 
     # Process chunks with tqdm progress bar
+    # Partial fit using iPCA 
     num_chunks = 0
     for idx, chunk in enumerate(tqdm(data_chunks, total=total_chunks, desc="Processing Chunks")):
         num_chunks += 1
 
         # Check if chunk already exists
         if os.path.exists(f'data/fingerprints_chunk_{idx}.pkl'):
-            print(f'Chunk {idx} already processed. Skipping.')
+            pass
         else:
             smiles_list, features = data_handler.extract_smiles_and_features(chunk)
 
             # Calculate fingerprints with progress bar
             fingerprints = fp_calculator.calculate_fingerprints(smiles_list)
-
+            
             # Save fingerprints
             with open(f'data/fingerprints_chunk_{idx}.pkl', 'wb') as f:
                 pickle.dump((fingerprints, smiles_list, features), f)
 
     # Load all fingerprints with tqdm progress bar
-    fingerprints, smiles_list, features = [], [], []
+    fingerprints, smiles_list, oh_features = [], [], []
     for idx in tqdm(range(num_chunks), desc="Loading Fingerprints"):
         with open(f'data/fingerprints_chunk_{idx}.pkl', 'rb') as f:
             fps_chunk, smiles_chunk, features_chunk = pickle.load(f)
             fingerprints.extend(fps_chunk)
             smiles_list.extend(smiles_chunk)
-            features.extend(features_chunk)
+            oh_features.extend(features_chunk)
 
-    # Perform Dimensionality Reduction
-    print("Performing Dimensionality Reduction...")
-    coordinates = reducer.fit_transform(fingerprints)
+    # data_dict = { 'smiles': smiles_list, 'fp': fingerprints, 'oh_features':oh_features} 
+    # # Perform Dimensionality Reduction
+    # print("Performing Dimensionality Reduction...")
+    # coordinates = reducer.fit_transform(fingerprints)
 
-    # Compile and export data
-    output_gen.compile_output(coordinates, smiles_list, features)
-    output_gen.export_data(OUTPUT_FILE_PATH)
+    # # Compile and export data
+    # output_gen.compile_output(coordinates, smiles_list, features)
+    # output_gen.export_data(OUTPUT_FILE_PATH)
 
 if __name__ == '__main__':
     main()
