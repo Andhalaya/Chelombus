d16d2cf681c39c2f00919df7e52c7d7f50982b80
afloresep <afloresep01@gmail.com>
2025-01-13 16:06:51 +0100

    update tmap to parallel runs

--- dynamic_tmap/scripts/tmap_pipeline.py
+++ dynamic_tmap/scripts/tmap_pipeline.py
@@ -1,19 +1,15 @@
 # File to run the Clustering pipeline
 import os
 import glob 
-import re 
 import sys
 import time
 import logging 
 import argparse
 import shutil
-import h5py
 import numpy as np
-from tqdm import tqdm
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from sklearn.decomposition import IncrementalPCA
 from memory_profiler import profile
-import gc
 import pandas as pd
 
 # This allows Python to recognize the src/ directory as a package and facilitates absolute imports from it.
@@ -23,6 +19,15 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from config import (INPUT_TMAP_PATH, OUTPUT_TMAP_PATH, CLUSTER_DATA_PATH, BASE_DIR,
                     LOGGING_LEVEL, LOGGING_FORMAT, LOG_FILE_PATH, N_JOBS, TMAP_K, TMAP_NAME)
 from src.tmap_generator import TmapGenerator, ClickhouseTMAP
+
+
+
+
+def format_time(seconds):
+      hours, rem = divmod(seconds, 3600)
+      minutes, seconds = divmod(rem, 60)
+      return f"{int(hours)} hours, {int(minutes)} minutes, {seconds:.2f} seconds"
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Process fingerprints with flexible options.")
@@ -47,6 +52,7 @@ def setup_logging(log_level, log_output):
     logging.info("Logging initialized")
 
 def main() -> None:
+    start = time.time()
     args = parse_arguments()
 
     # Set up logging based on the parsed arugments or config defaults
@@ -71,8 +77,35 @@ def main() -> None:
         tmap_generator.tmap_from_vectors() # -> This method uses a KNN for generating the TMAP from the PCA coordinates
 
     else:
+        import clickhouse_connect 
+
+        client = clickhouse_connect.get_client(host='localhost', port=8123)
+        # all_cluster_ids = client.query("SELECT DISTINCT cluster_id FROM clustered_enamine")
+        # all_cluster_ids = [cluster[0] for cluster in all_cluster_ids.result_rows] # Transform into lists from list of tuples
+
         clickhouse_tmap = ClickhouseTMAP() 
-        clickhouse_tmap.generate_tmap_from_cluster_id(args.l)
+
+        # max_workers = 8  # or 8, or however many CPU cores you want to use
+
+        # # Run in parallel
+        # with ProcessPoolExecutor(max_workers=max_workers) as executor:
+        #     logging.info("Starting Process")
+        #     # executor.map schedules each cluster_id on a separate process (up to max_workers)
+        #     # This returns an iterator of whatever the method returns.
+        #     results = executor.map(clickhouse_tmap.generate_tmap_from_cluster_id, all_cluster_ids, chunksize=20)
+        #     print("Batch done")
+        # # If your method returns something, you can iterate over it here
+        # for r in results:
+        #     logging.info(f"Result: {r}")
+
+        # clickhouse_tmap.generate_tmap_from_cluster_id(args.l)
+        total = 0
+        # for cluster_id in all_cluster_ids:
+        start = time.time()
+        clickhouse_tmap.generate_tmap_from_cluster_id(args.l, client)
+        end = time.time() 
+        print(f"Time per TMAP: {format_time(end - start)}")
+        end
 if __name__ == "__main__":
 
     logging.basicConfig(level=logging.INFO)
@@ -83,23 +116,20 @@ if __name__ == "__main__":
     logging.info("TMAP successfully generated")
 
     # # Define paths
-    # source_pattern = os.path.join('/home/afloresep/work/chelombus/backend', '*.html')
-    # destination_dir = os.path.join('/home/afloresep/work/chelombus/backend', 'static')
-    # os.makedirs(destination_dir, exist_ok=True)
-
-    # logging.info(f"Source pattern for HTML files: {source_pattern}")
-    # logging.info(f"Destination directory: {destination_dir}")
-
-    # # Move HTML files
-    # for file_path in glob.glob(source_pattern):
-    #     logging.info(f"Moving HTML file: {file_path} to {destination_dir}")
-    #     shutil.move(file_path, destination_dir)
-
-    # # Move JavaScript files
-    # source_pattern_js = os.path.join('/home/afloresep/work/chelombus/backend', '*.js')
-    # for file_path in glob.glob(source_pattern_js):
-    #     logging.info(f"Moving JS file: {file_path} to {destination_dir}")
-    #     shutil.move(file_path, destination_dir)
+    source_pattern = os.path.join('/home/afloresep/work/chelombus/dynamic_tmap', '*.html')
+    destination_dir = os.path.join('/home/afloresep/work/chelombus/maps', 'static')
+    os.makedirs(destination_dir, exist_ok=True)
+    logging.info(f"Source pattern for HTML files: {source_pattern}")
+    logging.info(f"Destination directory: {destination_dir}")
+    # Move HTML files
+    for file_path in glob.glob(source_pattern):
+        logging.info(f"Moving HTML file: {file_path} to {destination_dir}")
+        shutil.move(file_path, destination_dir)
+    # Move JavaScript files
+    source_pattern_js = os.path.join('/home/afloresep/work/chelombus/dynamic_tmap', '*.js')
+    for file_path in glob.glob(source_pattern_js):
+        logging.info(f"Moving JS file: {file_path} to {destination_dir}")
+        shutil.move(file_path, destination_dir)
 
     end_time = time.time()
     logging.info("All files moved successfully")
